ed4c58d2b1bd27a1b451c5f5ad3bfb50e6b4446f
Sudhansu Mohanty <sudhansumohanty02@gmail.com>
2026-09-05 14:22:42 -0400

    add GitHub Pages deployment workflow

--- scs-react/vite.config.js
+++ scs-react/vite.config.js
@@ -8,6 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/scs-website-sample/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
